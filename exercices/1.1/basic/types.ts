2c20b21668daf39c33b0119e5b7e53d11a8b5b69
Azouagh <azabd2003@gmail.com>
2024-10-30 17:42:06 +0100

    create: exercices 1.1

---
+++ exercices/1.1/basic/types.ts
@@ -0,0 +1,16 @@
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number
+}
+
+// interface PizzaToUpdate {
+//   title?: string;
+//   content?: string;
+// }
+
+// type NewPizza = Omit<Pizza, "id">;
+
+// export type { Pizza, NewPizza, PizzaToUpdate };
+export type {Film};
